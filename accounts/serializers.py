c985d8ed52920df62400a7e12e2ea4d033de379f
이지예 <sifbwi352@gmail.com>
2026-06-03 04:29:13 +0900

    feat: 회원가입 API 구현

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,20 +4,26 @@ from .models import User, UserProfile, UserHealthProfile, UserEnvironment
 
 class SignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
         model = User
-        fields = ["id", "email", "password", "nickname"]
+        fields = ["id", "email", "password", "password_confirm", "nickname"]
 
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("이미 사용 중인 이메일입니다.")
-        return value
+    def validate(self, data):
+        if data["password"] != data["password_confirm"]:
+            raise serializers.ValidationError({
+                "password_confirm": "비밀번호가 일치하지 않습니다."
+            })
+        return data
 
     def create(self, validated_data):
         email = validated_data["email"]
         password = validated_data["password"]
         nickname = validated_data["nickname"]
+
+        # password_confirm은 User 모델에 저장하는 값이 아니므로 제거
+        validated_data.pop("password_confirm")
 
         user = User.objects.create_user(
             username=email,
--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,22 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import User
 from .serializers import SignupSerializer
 
 
 class SignupView(APIView):
     def post(self, request):
+        email = request.data.get("email")
+
+        if User.objects.filter(email=email).exists():
+            return Response(
+                {
+                    "email": ["이미 사용 중인 이메일입니다."]
+                },
+                status=status.HTTP_409_CONFLICT
+            )
+
         serializer = SignupSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -16,7 +27,7 @@ class SignupView(APIView):
                 {
                     "message": "회원가입이 완료되었습니다.",
                     "user": {
-                        "id": user.id,
+                        "user_id": user.id,
                         "email": user.email,
                         "nickname": user.nickname,
                     }
